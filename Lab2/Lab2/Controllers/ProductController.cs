a7ba24f1d4bbf6350cf02c44f3299324c02396f5
racheldaley1 <46687970+racheldaley1@users.noreply.github.com>
2019-01-23 11:44:55 -0500

    Lab 2 Submission of ProductController

---
+++ Lab2/Lab2/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET /Product/
+        public string Index()
+
+        {
+            return "Product/Index is displayed.";
+        }
+        //
+        // GET /Product/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+        //
+        // GET /Product/Details/105
+        public string Details(int id)
+        {
+            string message = "Details displayed for Id=" + id;
+
+            return message;
+        }
+        //
+        // GET /Product/Location?zip=44124
+        public string Location(string zip)
+        {
+            string message =
+                HttpUtility.HtmlEncode("Location displayed for zip=" + zip);
+
+            return message;
+        }
+    }
+}
